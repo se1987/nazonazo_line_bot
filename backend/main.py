750e4e4698d55d6a1870db66882626722702b85b
se1987 <s.tsukuda1987@gmail.com>
2024-09-14 23:22:28 +0900

    フォローイベントの処理追加、回答の処理を追加

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
-from services.line_bot_service import send_start_message, handle_difficulty_selection, send_hint, reply_message
+from services.line_bot_service import send_start_message, handle_difficulty_selection, send_hint, reply_message, send_difficulty_selection_message
+from services.open_ai_service import check_user_answer
 import os
 import logging
 from dotenv import load_dotenv
@@ -27,13 +28,18 @@ async def webhook(request: Request):
     
     # 各イベントを処理
     for event in events:
-        if event['type'] == 'message' and event['message']['type'] == 'text':
+        if event['type'] == 'follow':  # ユーザーが初めて友だち追加した時
+            user_id = event['source']['userId']
+            send_start_message(user_id)
+            
+        elif event['type'] == 'message' and event['message']['type'] == 'text':
+            user_id = event['source']['userId']  # LINEユーザーのIDを取得
             reply_token = event['replyToken']  # リプライするためのトークン
             user_message = event['message']['text']  # ユーザーからのメッセージを取得
 
-            # 「謎謎出して」が送信された場合、難易度選択メッセージを表示
-            if user_message == "謎謎出して":
-                send_start_message(reply_token)
+            # 「スタート」メッセージが送信された場合、難易度選択ボタンを表示
+            if user_message == "スタート":
+                send_difficulty_selection_message(reply_token)
             # 難易度が選択された場合、対応する謎を出題
             elif user_message in ["簡単", "普通", "難しい"]:
                 handle_difficulty_selection(reply_token, user_message)
@@ -41,7 +47,12 @@ async def webhook(request: Request):
             elif user_message == "ヒント":
                 send_hint(reply_token, user_message)
             else:
-                # 回答メッセージが送信された場合、回答処理を行う
-                reply_message(reply_token, "回答を入力してください。")
+                # 「もう一度」が送信された場合、最初にスタートボタンを表示
+                if user_message == "もう一度":
+                    handle_difficulty_selection(reply_token, user_message)
+                else:
+                    # 回答メッセージが送信された場合、回答処理を行う
+                    result_message = check_user_answer(user_id, user_message)  # ユーザーIDに基づいて回答を判定
+                    reply_message(reply_token, result_message)  # 判定結果を送信
 
     return {"status": "ok"}
